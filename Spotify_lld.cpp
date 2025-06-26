1b47c94052d4d30a7e16ce8291c316dceafe1b47
Ajay Gound <164065148+Kenpacchi@users.noreply.github.com>
2025-06-26 15:06:06 +0530

    Update Spotify_lld.cpp

--- Spotify_lld.cpp
+++ Spotify_lld.cpp
@@ -1,43 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-// Forward declaration
-class Songs;
-
-class Songs {
-private:
-    long songId;
-    string songName;
-    long views;
-    string songArtistName;
-    string uploadDate;
-
-public:
-    // Getters
-    long getSongId() const { return songId; }
-    string getSongName() const { return songName; }
-    long getSongViews() const { return views; }
-    string getArtistName() const { return songArtistName; }
-    string getUploadDate() const { return uploadDate; }
-
-    // Setters
-    void setSongId(long id) { songId = id; }
-    void setSongName(const string& name) { songName = name; }
-    void setSongViews(long v) { views = v; }
-    void setArtistName(const string& name) { songArtistName = name; }
-    void setUploadDate(const string& date) { uploadDate = date; }
-};
-
-class User {
-private:
-    long id;
-    string userName;
-    string userPhoneNumber;
-    string userEmail;
-    string userPassword;
-    vector<Songs> userPlaylist;
-
-public:
+class User{
+   private:
+   long id ;// primary key
+   string userName;
+   string userPhoneNumber;
+   string userEmail;
+   string userPassword;
+   vector<Songs>userPlaylist;
+   public:
     // Getters
     long getId() const { return id; }
     string getUserName() const { return userName; }
@@ -54,14 +25,36 @@ public:
     void setPassword(const string& password) { userPassword = password; }
     void setPlaylist(const vector<Songs>& playlist) { userPlaylist = playlist; }
 };
+class Songs{
+   private:
+   long songId; // primary key
+   long artistId;
+   long movieId;// foriegn Key
+   string songName;
+   long views;
+   string songArtistName;
+   string uploadDate;
+   public:
+    // Getters
+    long getSongId() const { return songId; }
+    string getSongName() const { return songName; }
+    long getSongViews() const { return views; }
+    string getArtistName() const { return songArtistName; }
+    string getUploadDate() const { return uploadDate; }
 
-class Artist {
-private:
-    long artistId;
-    string artistName;
-    vector<Songs> artistSongs;
-
-public:
+    // Setters
+    void setSongId(long id) { songId = id; }
+    void setSongName(const string& name) { songName = name; }
+    void setSongViews(long v) { views = v; }
+    void setArtistName(const string& name) { songArtistName = name; }
+    void setUploadDate(const string& date) { uploadDate = date; }
+};
+class Artist{
+   private:
+   long artistId; // primary key
+   string artistName;
+   vector<Songs>artistSongs;
+   public:
     // Getters
     long getArtistId() const { return artistId; }
     string getArtistName() const { return artistName; }
@@ -72,13 +65,11 @@ public:
     void setArtistName(const string& name) { artistName = name; }
     void setArtistSongs(const vector<Songs>& songs) { artistSongs = songs; }
 };
-
-class Movie {
-private:
-    long movieId;
-    vector<Songs> movieSongs;
-
-public:
+class Movie{
+   private:
+   long movieId;// primary key
+   vector<Songs>movieSongs;
+   public:
     // Getters
     long getMovieId() const { return movieId; }
     vector<Songs>& getMovieSongs() { return movieSongs; }
@@ -87,183 +78,170 @@ public:
     void setMovieId(long id) { movieId = id; }
     void setMovieSongs(const vector<Songs>& songs) { movieSongs = songs; }
 };
+// Main Class
+class Spotify{
+   public:
+   map<string,User>userDb;  //USER DATABASE
+   vector<Songs>songDb;     //SONG DATABASE
+   map<string,Artist>artistDb;
+   map<string,Movie>movieDb;
+   map<string,Songs>songS;
+   long failed;
+   Spotify(){
+      failed=0;
+   }
+   public:
+   void checkCredential(User user){
+      if(userDb.find(user.getPhoneNumber())!=userDb.end()){
+         cout<<"User Exist Login please:"<<endl;
+         loginPage(user);
+         return;
+      }
+      signUpPage(user);
+   }
+   void loginPage(User user){
+      string actualPass=userDb[user.getPhoneNumber()].getPassword();
+      if(actualPass==user.getPassword()){
+         cout<<"Login Success!!"<<endl;
+         userInterface(user);
+         return;
+      }
+      failed++;
+      if(failed<=5){
+         cout<<"Please Enter Correct Password"<<'\n';
+         string password;
+         cin>>password;
+         user.setPassword(password);
+         loginPage(user);
+         return;
+      }else{
+         cout<<"Login Failed Try again after 24 Hours"<<endl;
+      }
+   }
+   void signUpPage(User user){
+      string phoneNumber = user.getPhoneNumber();
+      userDb[phoneNumber]=user;
+      cout<<"SignUp  Success"<<endl;
+   }
+   void streamSong(Songs song){
+      // we will use audio player here....
+   }
+   void showSongs(Songs song){
+      cout<<song.getSongName()<<endl;
+      cout<<song.getArtistName()<<endl;
+      cout<<song.getSongViews()<<endl;
+      cout<<song.getUploadDate()<<endl;
+      streamSong(song);
+   }
+   vector<Songs> trendingSongs(){
+      // trending page
+      vector<Songs>trending=songDb;
+      sort(trending.begin(),trending.end(),[&](auto &a,auto&b){
+         if(a.getSongViews()==b.getSongViews){
+            return a.getUploadDate()<b.getUploadDate();
+         }
+         return a.getSongViews()<b.getSongViews;
+      });
+      return trending;
+   }
+   vector<Songs>recommendations(User user,string artistName ){
+            // Recommendation
+      vector<Songs>userPlaylist=user.getPlaylist();
+      map<string,bool>checkAlreadyShown;
+      for(auto userSong:userPlaylist){
+         string artistName=userSong.getArtistName();
+         if(checkAlreadyShown.find(artistName)!=checkAlreadyShown.end()){
+            continue;
+         }
+         checkAlreadyShown[artistName]=true;
+      }
+      return artistDb[artistName].getArtistSongs();
+   }
+   void userInterface(User user){
+      // User searching
+      string name;
+      cin>>name;
+      vector<Songs>listOfSongsOfMovie=searchSongByMovieName(name);
+      vector<Songs>listOfSongsbyArtist=searchSongByArtistName(name);
+      Songs songByName=searchSongByName(name);
 
-class Spotify {
-private:
-    map<string, User> userDb;
-    vector<Songs> songDb;
-    map<string, Artist> artistDb;
-    map<string, Movie> movieDb;
-    map<string, Songs> songMap;
-    long failed;
+      // playlist Creation
+      vector<Songs>newUserPlaylist;
+      user.setPlaylist(newUserPlaylist);
 
-public:
-    Spotify() {
-        failed = 0;
-    }
-
-    void checkCredential(User& user) {
-        if (userDb.find(user.getPhoneNumber()) != userDb.end()) {
-            cout << "User exists. Please login:\n";
-            loginPage(user);
-        } else {
-            signUpPage(user);
-        }
-    }
-
-    void loginPage(User& user) {
-        string phone = user.getPhoneNumber();
-        string actualPass = userDb[phone].getPassword();
-        if (actualPass == user.getPassword()) {
-            cout << "Login Success!!\n";
-            userInterface(user);
-        } else {
-            failed++;
-            if (failed <= 5) {
-                cout << "Incorrect password. Try again:\n";
-                string password;
-                cin >> password;
-                user.setPassword(password);
-                loginPage(user);
-            } else {
-                cout << "Login Failed. Try again after 24 hours.\n";
-            }
-        }
-    }
-
-    void signUpPage(User& user) {
-        string phoneNumber = user.getPhoneNumber();
-        userDb[phoneNumber] = user;
-        cout << "SignUp Success!\n";
-    }
-
-    void streamSong(const Songs& song) {
-        cout << "[Streaming] " << song.getSongName() << " by " << song.getArtistName() << "\n\n";
-    }
-
-    void showSongs(const Songs& song) {
-        cout << "Name: " << song.getSongName() << "\n";
-        cout << "Artist: " << song.getArtistName() << "\n";
-        cout << "Views: " << song.getSongViews() << "\n";
-        cout << "Upload Date: " << song.getUploadDate() << "\n";
-        streamSong(song);
-    }
-
-    void userInterface(User& user) {
-        // Trending
-        vector<Songs> trending = songDb;
-        sort(trending.begin(), trending.end(), [&](const Songs& a, const Songs& b) {
-            if (a.getSongViews() == b.getSongViews()) {
-                return a.getUploadDate() < b.getUploadDate();
-            }
-            return a.getSongViews() > b.getSongViews(); // descending
-        });
-
-        cout << "\n--- Trending Songs ---\n";
-        for (const auto& song : trending) {
-            showSongs(song);
-        }
-
-        // Recommendations
-        cout << "\n--- Recommended Songs ---\n";
-        map<string, bool> shown;
-        for (const auto& userSong : user.getPlaylist()) {
-            string artist = userSong.getArtistName();
-            if (shown[artist]) continue;
-            shown[artist] = true;
-            if (artistDb.find(artist) != artistDb.end()) {
-                for (const auto& song : artistDb[artist].getArtistSongs()) {
-                    showSongs(song);
-                }
-            }
-        }
-
-        // Searching
-        cout << "\nSearch by song, movie or artist name: ";
-        string name;
-        cin >> name;
-        searchSongs(user, name);
-
-        // Playlist creation
-        user.setPlaylist({}); // clear old playlist
-
-        cout << "\nAdd songs to your playlist (up to 5 songs):\n";
-        for (int i = 0; i < 5; ++i) {
-            string songName;
-            cout << "Enter song name: ";
-            cin >> songName;
-            if (songMap.find(songName) != songMap.end()) {
-                user.getPlaylist().push_back(songMap[songName]);
-                cout << "Added!\n";
-            } else {
-                cout << "Song not found.\n";
-            }
-        }
-    }
-
-    void searchSongs(User& user, const string& name) {
-        cout << "\n--- Search Results ---\n";
-        if (movieDb.find(name) != movieDb.end()) {
-            for (const auto& song : movieDb[name].getMovieSongs()) {
-                showSongs(song);
-            }
-        }
-        if (artistDb.find(name) != artistDb.end()) {
-            for (const auto& song : artistDb[name].getArtistSongs()) {
-                showSongs(song);
-            }
-        }
-        if (songMap.find(name) != songMap.end()) {
-            showSongs(songMap[name]);
-        }
-    }
-
-    // Dummy data for testing
-    void loadDummyData() {
-        Songs s1, s2;
-        s1.setSongId(1);
-        s1.setSongName("SongA");
-        s1.setSongViews(100);
-        s1.setArtistName("ArtistX");
-        s1.setUploadDate("2024-01-01");
-
-        s2.setSongId(2);
-        s2.setSongName("SongB");
-        s2.setSongViews(200);
-        s2.setArtistName("ArtistY");
-        s2.setUploadDate("2024-06-01");
-
-        songDb.push_back(s1);
-        songDb.push_back(s2);
-        songMap[s1.getSongName()] = s1;
-        songMap[s2.getSongName()] = s2;
-
-        Artist a1;
-        a1.setArtistId(101);
-        a1.setArtistName("ArtistX");
-        a1.setArtistSongs({s1});
-        artistDb["ArtistX"] = a1;
-
-        Artist a2;
-        a2.setArtistId(102);
-        a2.setArtistName("ArtistY");
-        a2.setArtistSongs({s2});
-        artistDb["ArtistY"] = a2;
-    }
+      // Add Songs To Playlist
+      for(int i=0;i<100;i++){// limit of playlist size...
+      string songName;
+      cin>>songName;
+      user.getPlaylist().push_back(songS[songName]);
+      }
+   }
+   Songs searchSongByName(string name){
+      if(songS.find(name)!=songS.end()){
+            return songS[name];
+      }
+      return {};
+   }
+   vector<Songs>searchSongByMovieName(string name){
+      if(movieDb.find(name)!=movieDb.end()){
+         vector<Songs>byMovieName=movieDb[name].getMovieSongs();
+         return byMovieName;
+      }
+      return {};
+   }
+   vector<Songs> searchSongByArtistName(string name){
+      if(artistDb.find(name)!=artistDb.end()){
+         vector<Songs>byArtistName=artistDb[name].getArtistSongs();
+         return byArtistName;
+      }
+      return {};
+  }
 };
-
 int main() {
-    Spotify app;
-    app.loadDummyData();
+    Spotify  app;
 
-    User user;
-    user.setId(1);
-    user.setUserName("Ajay");
-    user.setPhoneNumber("1234567890");
-    user.setEmail("ajay@example.com");
-    user.setPassword("pass123");
+    // Sample Data Initialization
+    Songs song1;
+    song1.setSongId(1);
+    song1.setSongName("SongA");
+    song1.setArtistName("ArtistX");
+    song1.setSongViews(1000);
+    song1.setUploadDate("2025-06-01");
 
-    app.checkCredential(user);
+    Songs song2;
+    song2.setSongId(2);
+    song2.setSongName("SongB");
+    song2.setArtistName("ArtistX");
+    song2.setSongViews(2000);
+    song2.setUploadDate("2025-06-15");
+
+    Artist artist;
+    artist.setArtistId(101);
+    artist.setArtistName("ArtistX");
+    artist.setArtistSongs({song1, song2});
+
+    Movie movie;
+    movie.setMovieId(201);
+    movie.setMovieSongs({song1});
+
+    // Manually inserting into DB
+    app.songS["SongA"] = song1;
+    app.songS["SongB"] = song2;
+    app.artistDb["ArtistX"] = artist;
+    app.movieDb["MovieMovie"] = movie;
+    app.songDb.push_back(song1);
+    app.songDb.push_back(song2);
+
+    // Creating a new user
+    User newUser;
+    newUser.setId(1);
+    newUser.setUserName("Ajay");
+    newUser.setPhoneNumber("1234567890");
+    newUser.setEmail("ajay@example.com");
+    newUser.setPassword("pass123");
+
+    // Signup/Login flow
+    app.checkCredential(newUser);
 
     return 0;
 }
